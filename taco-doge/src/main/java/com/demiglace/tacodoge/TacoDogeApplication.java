8d31d0bd0fef5d25ffb4007160cb74125bc299d0
demiglace0505 <demiglace0505@gmail.com>
2022-04-11 00:22:51 +0800

    todo: 2.3 validating form input

---
+++ taco-doge/src/main/java/com/demiglace/tacodoge/TacoDogeApplication.java
@@ -0,0 +1,13 @@
+package com.demiglace.tacodoge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoDogeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoDogeApplication.class, args);
+	}
+
+}
---
+++ taco-doge/src/test/java/com/demiglace/tacodoge/TacoDogeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demiglace.tacodoge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoDogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
